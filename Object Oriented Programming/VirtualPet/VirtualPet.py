1e84ad57991ce8fd10c1fbc303b52637ba39283e
Maria Fernanda Caetano <nanda2001caetano@gmail.com>
2023-03-09 23:46:48 -0300

    Doing a new project based on a class exercice

---
+++ Object Oriented Programming/VirtualPet/VirtualPet.py
@@ -0,0 +1,20 @@
+# Criar uma classe chamada Bichindo Virutal, que modele um Tamagoshi.
+# Atributos obrigatórios: nome, fome, saúde e idade
+# Métodos obrigatórios: alterarNome, alterarFome, alterarSaude, alterarIdade
+# Métodos obrigatórios: mostrarNome, mostraFome, mostrarSaude, mostrarIdade
+# Atributo umor: combinado entre os atributos fome x saude, pode mudar conforme fome e saude mudam.
+
+# Permitir que o usuário especifique quanto de comida fornecer ao bichinho
+# Permitir que o usuário defina quanto tempo brincar com o bichinho
+# Fazer testes que afetem o quão rapidamente os níveis de fome e tédio caem
+
+# Porta escondida: utilizar do método mágico "__str__" para apresentar as informações do bichinho
+
+# Criar uma fazenda de bichinhos virtuais, instanciando vários objetos.
+# Manter o controle deles através de uma lista.
+# Fazer com que o usuário tome conta de vários bichinhos.
+# Dar valor inicial aleatório de fome e tédio a cada bichinho.
+
+class VirutalPet():
+    
+    
---
+++ Object Oriented Programming/VirtualPet/VirtualPetGui.py
@@ -0,0 +1,44 @@
+from tkinter import *
+from tkinter.ttk import *
+
+
+def new_game():
+    # choose new game name and open file
+    new_game_file = Tk()
+    new_game_file.title("Farm: New Game")
+    new_game_file.geometry("200x200")
+    
+    new_name_text = "Text your name to load game"
+    Label(new_game_file, text=new_name_text).pack()
+    new_game_name = StringVar()
+    Entry(new_game_file, textvariable=new_game_name).pack()
+    
+    print (str(new_game_name.get()))
+    
+       
+    def new_game_root():
+        new_game_win = Tk()
+        new_game_win.title(f"{new_game_name}'s Farm")
+        new_game_win.geometry('400x200')
+        new_game_file.destroy()
+        
+    Button(new_game_file, text="Play", command=new_game_root).pack()
+    
+main_window = Tk()
+main_window.title("Virtual Farm")
+main_window.geometry("350x350")
+
+# Texto de entrada
+texto = """Choose an option
+(change this later)
+"""
+t = Text(main_window, height=5, width=30)
+t.insert(INSERT, texto)
+t.pack()
+
+# Botões do menu iniciar
+new_game = Button(main_window, text="New Game", command=new_game).pack()
+load_game = Button(main_window, text="Load Game").pack()
+
+mainloop()
+

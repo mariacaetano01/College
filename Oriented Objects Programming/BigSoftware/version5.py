f6e7482e43011be0db6f55caa14662ce9911220f
Maria Caetano <nanda2001caetano@gmail.com>
2023-06-29 21:56:59 -0300

    Updated version 5

--- Oriented Objects Programming/BigSoftware/version5.py
+++ Oriented Objects Programming/BigSoftware/version5.py
@@ -48,27 +48,31 @@ class InstrumentType(Enum):
 
 # MODIFICATION: new class instrument 
 # Now all instruments are objects of this class, without any subclass
-class Instrument:
+class Instrument():
     def __init__(self, serial_number, price, spec):
-        self.serial_number = serial_number
+        self.serialNum = serial_number
         self.price = price
-        self.spec = spec
+        self.spec = spec #spec is an InstrumentSpec object
 
-    def get_serial_number(self):
-        return self.serial_number
+    # methods to return intruments specifications
+    def getSerial_number(self):
+        return self.serialNum
 
-    def get_price(self):
+    def getPrice(self):
         return self.price
 
-    def set_price(self, new_price):
+    def setPrice(self, new_price):
         self.price = new_price
 
-    def get_spec(self):
+    def getSpec(self):
         return self.spec
+    
+    def __str__ (self):
+        return f"SERIAL NUMBER: {self.serialNum}\nPRICE: R${self.price}\n{self.spec}"
 
 # MODIFICATION: new class InstrumentSpec
 # Now all instruments' specifications are objects of this class, without any subclass
-class InstrumentSpec:
+class InstrumentSpec():
     def __init__(self, properties=None):
         if properties is None:
             self.properties = {}
@@ -77,34 +81,34 @@ class InstrumentSpec:
 
     # MODIFICATION
     # method to retrun instruments' specific property
-    def get_property(self, property_name):
+    def getProperty(self, property_name):
         return self.properties.get(property_name)
 
     # MODIFICATION
     # new method to return instruments's properties
-    def get_properties(self):
+    def getProperties(self):
         return self.properties
 
     # MODIFICATION
     # new query method to match intruments during the searhcing
     def matches(self, other_spec):
-        for property_name in other_spec.get_properties():
-            if self.properties.get(property_name) != other_spec.get_property(property_name):
+        for property_name in other_spec.getProperties():
+            if self.properties.get(property_name) != other_spec.getProperty(property_name):
                 return False
         return True
 
-# class Invenotory, to store intruments objects
-class Inventory:
+# class Inventory, to store intruments objects
+class Inventory():
     def __init__(self):
         self.inventory = []
 
     # method to include new instruments objects into the inventory list object
-    def add_instrument(self, serial_number, price, spec):
+    def addInstrument(self, serial_number, price, spec):
         instrument = Instrument(serial_number, price, spec)
         self.inventory.append(instrument)
 
      # search guitars or mandolins by its serial number
-    def get_instrument(self, serial_number):
+    def getInstrument(self, serial_number):
         for instrument in self.inventory:
             if instrument.get_serial_number() == serial_number:
                 return instrument
@@ -115,76 +119,76 @@ class Inventory:
         # search_spec is a InstrumentSpec object
         matching_instruments = []
         for instrument in self.inventory:
-            if instrument.get_spec().matches(search_spec):
+            if instrument.getSpec().matches(search_spec):
                 matching_instruments.append(instrument)
         return matching_instruments
     
 # MODIFICATION: new method with ready dictionaries to use as Specifications
 def initialize_inventory(inventory):
     properties = {
-        "instrumentType": InstrumentType.GUITAR.value,
-        "builder": Builder.COLLINGS.value,
-        "model": "CJ",
-        "type": Type.ACOUSTIC.value,
-        "numstrings": 6,
-        "topwood": Wood.INDIAN_ROSEWOOD.value,
-        "backwood": Wood.SITKA.value
+        "Instrument Type": InstrumentType.GUITAR.value,
+        "Builder": Builder.COLLINGS.value,
+        "Model": "CJ",
+        "Type": Type.ACOUSTIC.value,
+        "Num Strings": 6,
+        "Top Wood": Wood.INDIAN_ROSEWOOD.value,
+        "Back Wood": Wood.SITKA.value
     }
-    inventory.add_instrument("11277", 3999.95, InstrumentSpec(properties))
+    inventory.addInstrument("11277", 3999.95, InstrumentSpec(properties))
 
     properties = {
-        "instrumentType": InstrumentType.GUITAR.value,
-        "builder": Builder.GIBSON.value,
-        "model": "Les Paul",
-        "type": Type.ELECTRIC.value,
-        "numstrings": 6,
-        "topwood": Wood.MAPLE.value,
-        "backwood": Wood.MAPLE.value
+        "Instrument Type": InstrumentType.GUITAR.value,
+        "Builder": Builder.GIBSON.value,
+        "Model": "Les Paul",
+        "Type": Type.ELECTRIC.value,
+        "Num Strings": 6,
+        "Top Wood": Wood.MAPLE.value,
+        "Back Wood": Wood.MAPLE.value
     }
-    inventory.add_instrument("70108276", 2295.95, InstrumentSpec(properties))
+    inventory.addInstrument("70108276", 2295.95, InstrumentSpec(properties))
 
     properties = {
-        "instrumentType": InstrumentType.MANDOLIN.value,
-        "builder": Builder.GIBSON.value,
-        "model": "F5-G",
-        "type": Type.ACOUSTIC.value,
-        "topwood": Wood.MAPLE.value,
-        "backwood": Wood.MAPLE.value,
-        "style": Style.A.value
+        "Instrument Type": InstrumentType.MANDOLIN.value,
+        "Builder": Builder.GIBSON.value,
+        "Model": "F5-G",
+        "Type": Type.ACOUSTIC.value,
+        "Top Wood": Wood.MAPLE.value,
+        "Back Wood": Wood.MAPLE.value,
+        "Style": Style.A.value
     }
-    inventory.add_instrument("9019920", 5495.99, InstrumentSpec(properties))
+    inventory.addInstrument("9019920", 5495.99, InstrumentSpec(properties))
 
     properties = {
-        "instrumentType": InstrumentType.BANJO.value,
-        "builder": Builder.GIBSON.value,
-        "model": "RB-3",
-        "type": Type.ACOUSTIC.value,
-        "numstrings": 5,
-        "backwood": Wood.MAPLE.value
+        "Instrument Type": InstrumentType.BANJO.value,
+        "Builder": Builder.GIBSON.value,
+        "Model": "RB-3",
+        "Type": Type.ACOUSTIC.value,
+        "Num Strings": 5,
+        "Back Wood": Wood.MAPLE.value
     }
-    inventory.add_instrument("8900231", 2945.95, InstrumentSpec(properties))
+    inventory.addInstrument("8900231", 2945.95, InstrumentSpec(properties))
 
 # FIFTH VERSION TESTS - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    inventory = Inventory()
-    initialize_inventory(inventory)
+inventory = Inventory()
+initialize_inventory(inventory)
 
-    properties = {
-        "builder": Builder.GIBSON.value,
-        "backwood": Wood.MAPLE.value
-    }
+properties = {
+    "Builder": Builder.GIBSON.value,
+    "Back Wood": Wood.MAPLE.value
+}
     
-    client_spec = InstrumentSpec(properties)
-    matching_instruments = inventory.search(client_spec)
+client_spec = InstrumentSpec(properties)
+matching_instruments = inventory.search(client_spec)
     
-    if matching_instruments:
-        for instrument in matching_instruments:
-            spec = instrument.get_spec()
-            print(spec.get_property("instrumentType"), "com as seguintes propriedades:")
-            for property_name, property_value in spec.get_properties().items():
-                if property_name == "instrumentType":
-                    continue
-                print (f"{property_name}: {property_value}")
-            print (f"It can be yours for just $ {instrument.get_price()}")
-            print()
-    else:
-        print("\033[34m\nNothing Found\033[0m\n")
+if matching_instruments:
+    for instrument in matching_instruments:
+        spec = instrument.getSpec()
+        print(f"\033[36m{spec.getProperty('Instrument Type')}\033[0m")
+        for property_name, property_value in spec.getProperties().items():
+            if property_name == "Instrument Type":
+                continue
+            print (f"\033[31m{property_name}:\033[0m {property_value}")
+        print (f"It can be yours for just $\033[32m{instrument.getPrice()}\033[0m")
+        print()
+else:
+    print("\033[34m\nNothing Found\033[0m\n")
